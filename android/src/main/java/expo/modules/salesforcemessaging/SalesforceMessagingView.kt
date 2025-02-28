13c71c2443ce082f8c38d38208fb728972e83448
Paul Goergen <pagkt2@gmail.com>
2025-02-27 22:38:31 -0800

    Android works, in like 5 mins!  FML

--- android/src/main/java/expo/modules/salesforcemessaging/SalesforceMessagingView.kt
+++ android/src/main/java/expo/modules/salesforcemessaging/SalesforceMessagingView.kt
@@ -5,6 +5,7 @@ import android.content.Context
 import expo.modules.kotlin.AppContext
 import expo.modules.kotlin.views.ExpoView
 import com.salesforce.android.smi.core.*
+import com.salesforce.android.smi.network.data.domain.prechat.PreChatField
 import com.salesforce.android.smi.ui.*
 import java.net.URL
 import java.util.UUID
@@ -15,6 +16,29 @@ class SalesforceMessagingView(context: Context, appContext: AppContext) : ExpoVi
     val coreConfig = CoreConfiguration(url, "00DHs00000CnigT", "Messaging_for_Mobile")
     val conversationID = UUID.randomUUID()
     val config = UIConfiguration(coreConfig, conversationID)
+
+  // Create a core client from a config
+      val coreClient = CoreClient.Factory.create(context, config)
+
+  // Register a provider
+      coreClient.registerHiddenPreChatValuesProvider( object : PreChatValuesProvider {
+
+        // Implement the setValues method
+        override suspend fun setValues(input: List<PreChatField>): List<PreChatField> {
+
+          // Iterate through all the pre-chat fields
+          input.forEach {
+
+            // Specify the value for each field mapping
+            if (it.name == "Driver_External_ID") {
+              it.userInput = "driver_LPPD93VQJH"
+            }
+          }
+
+          // Return the same list, but with updated values set
+          return input
+        }
+      })
 
     val uiClient = UIClient.Factory.create(config)
     uiClient.openConversationActivity(context)
